5487462c893d1c3657de7cb3dd1dd33f52133f19
Nata <155917350+nkvantaliani@users.noreply.github.com>
2025-11-17 20:22:18 +0400

    exam

---
+++ Exam/src/Main.java
@@ -0,0 +1,124 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // 1 - b
+        // 2 - c
+        // 3 - d
+        // 4 - c
+        // 5 - a
+        // 6 - c
+        // 7 - a
+        // 8 - a
+        // 9 - a
+        // 10 - c
+        // 11 - c
+        // 12 - c
+        // 13 - a
+        // 14 - a
+        // 15 - b
+
+
+        // 16
+//            int[] prices =  {1,5,9,2,10,7};
+//            int minPrice = prices[0];
+//            int maxPrice = prices[0];
+//            int minIndex = 0;
+//            int maxIndex = 0;
+//            for (int i = 0; i <prices.length ; i++) {
+//                if(prices[i]<minPrice){
+//                    minPrice = prices[i];
+//                    minIndex = i;
+//                }if(prices[i]>maxPrice){
+//                    maxPrice = prices[i];
+//                    maxIndex = i;
+//                }
+//            }
+//            System.out.println(minIndex+" მაღაზიაში ყველაზე იაფია და ღირს "+ minPrice);
+//            System.out.println(maxIndex + " მაღაზიაში ყველაზე ძვირია და ღირს " + maxPrice);
+
+        // 17
+//        for (int i = 0; i <= 100; i++) {
+//            if(i%35==0){
+//                System.out.println("HEYHOW");
+//            } else if (i%5 == 0) {
+//                System.out.println("HEY");
+//            }else if (i%7==0){
+//                System.out.println("HOW");
+//            }else {
+//                System.out.println(i);
+//            }
+//        }
+
+        // 18
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("enter row number: ");
+//        int rows = input.nextInt();
+//        System.out.print("enter column number: ");
+//        int columns = input.nextInt();
+//        Random random = new Random();
+//
+//        int[][] arr = new int[rows][columns];
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                arr[i][j] = random.nextInt(100);
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(arr));
+//
+//        int minNum = arr[0][0];
+//        int maxNum = arr[0][0];
+//        int minRow = 0;
+//        int minColumn = 0;
+//        int maxRow = 0;
+//        int maxColumn = 0;
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                if(arr[i][j] < minNum){
+//                    minNum = arr[i][j];
+//                    minRow = i;
+//                    minColumn = j;
+//                }
+//                if(arr[i][j] > maxNum){
+//                    maxNum = arr[i][j];
+//                    maxRow = i;
+//                    maxColumn = j;
+//                }
+//            }
+//        }
+//        System.out.println();
+//        System.out.println("greatest number is "+ maxNum +" its index is " + maxRow+ " " + maxColumn);
+//        System.out.println("smallest number is "+ minNum +" its index is " + minRow+ " " + minColumn);
+//
+//    // 19
+//        int[][] arr = { {1,2,3,4},
+//                        {1,2,3,4},
+//                        {1,2,3,4}
+//        };
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if(i==arr.length - i -1){
+//                sum+= arr[i][i];
+//            }
+//            else {
+//                sum+= arr[i][i];
+//                sum += arr[i][arr.length-i-1];
+//            }
+//
+//        }
+//        System.out.println(sum);
+
+        // 20
+//        int[][] arr ={
+//                {1,2,3},
+//                {4,5,6},
+//                {7,8,9}
+//        };
+        //ಥ_ಥ
+    }
+}
