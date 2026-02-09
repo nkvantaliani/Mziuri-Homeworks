0e478c7350a63968be61301dd8b8c8419ff201d0
Nata <155917350+nkvantaliani@users.noreply.github.com>
2026-02-09 04:59:58 +0400

    files homework

--- Files/src/Hw5.java
+++ Files/src/Hw5.java
@@ -1,2 +1,36 @@
+import java.io.*;
+
 public class Hw5 {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader file1 = new BufferedReader(new FileReader("files/hw4.txt"));
+            BufferedReader file2 = new BufferedReader(new FileReader("files/hwCAP.txt"));
+            BufferedWriter res = new BufferedWriter(new FileWriter("files/result.txt"));
+
+            res.write("first file");
+            res.newLine();
+
+            String line;
+            while ((line = file1.readLine()) != null) {
+                res.write(line);
+                res.newLine();
+            }
+            res.newLine();
+            res.write("second file");
+            res.newLine();
+
+            while ((line = file2.readLine()) != null) {
+                res.write(line);
+                res.newLine();
+            }
+            file1.close();
+            file2.close();
+            res.close();
+
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+    }
 }
+
